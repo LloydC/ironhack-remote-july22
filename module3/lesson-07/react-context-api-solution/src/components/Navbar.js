06bb15b4365ee83817bf422b922c02087d6495f8
LloydC <lloydchambrier@gmail.com>
2022-09-06 08:59:57 +0200

    refactor: lesson-07 small changes

--- module3/lesson-07/react-context-api-solution/src/components/Navbar.js
+++ module3/lesson-07/react-context-api-solution/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import { useContext } from 'react'; // <== ADD
-import { ThemeContext } from './../context/theme.context'; 
+import { ThemeContext } from '../context/theme.context'; 
  
 function Navbar() {
     const { theme, toggleTheme } = useContext(ThemeContext);
--- module3/lesson-07/react-context-api-solution/src/pages/HomePage.js
+++ module3/lesson-07/react-context-api-solution/src/pages/HomePage.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { ThemeContext } from './../context/theme.context';
+import { ThemeContext } from '../context/theme.context';
 
 const profileImg = 'https://i.imgur.com/i1gsj0v.png';
  
